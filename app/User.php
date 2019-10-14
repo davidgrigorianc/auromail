833832bc2fb4b9cd1fab167a76ae39af70a8a020
davit <david.grigorianc@gmail.com>
2019-10-14 21:02:09 +0400

    added authentication

--- app/User.php
+++ app/User.php
@@ -10,6 +10,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const ADMIN_TYPE = 'admin';
+    const DEFAULT_TYPE = 'user';
+    
+
     /**
      * The attributes that are mass assignable.
      *
@@ -36,4 +40,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdmin()    {        
+        return $this->type === self::ADMIN_TYPE;    
+    }
 }
